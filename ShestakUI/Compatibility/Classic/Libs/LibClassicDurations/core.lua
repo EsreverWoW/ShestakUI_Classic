856da01829b270b32d3dd739f196139157fcf974
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-08-28 23:01:48 -0500

    Update LCD

--- ShestakUI/Compatibility/Classic/Libs/LibClassicDurations/core.lua
+++ ShestakUI/Compatibility/Classic/Libs/LibClassicDurations/core.lua
@@ -1,5 +1,3 @@
-if _G.WOW_PROJECT_ID ~= _G.WOW_PROJECT_CLASSIC then return end
-
 --[================[
 LibClassicDurations
 Author: d87
@@ -63,7 +61,7 @@ Usage example 2:
 --]================]
 
 
-local MAJOR, MINOR = "LibClassicDurations", 12
+local MAJOR, MINOR = "LibClassicDurations", 13
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
@@ -412,15 +410,19 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(event)
     spellID, spellName, spellSchool, auraType, amount = CombatLogGetCurrentEventInfo()
 
     if auraType == "BUFF" or auraType == "DEBUFF" then
+        local isSrcPlayer = bit_band(srcFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0
+
         if spellID == 0 then
             -- so not to rewrite the whole thing to spellnames after the combat log change
             -- just treat everything as max rank id of that spell name
-            spellID = spellNameToID[spellName]
-            if not spellID then
+            if isSrcPlayer then
+                spellID = spellNameToID[spellName]
+            else
                 spellID = NPCspellNameToID[spellName]
-                if not spellID then
-                    return
-                end
+            end
+
+            if not spellID then
+                return
             end
         end
 
