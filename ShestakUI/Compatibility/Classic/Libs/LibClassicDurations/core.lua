7bf87ce8d3f7ca1c153f872d88e4a9f0e44168da
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-09-24 15:31:00 -0500

    Update LCD

--- ShestakUI/Compatibility/Classic/Libs/LibClassicDurations/core.lua
+++ ShestakUI/Compatibility/Classic/Libs/LibClassicDurations/core.lua
@@ -61,7 +61,7 @@ Usage example 2:
 --]================]
 if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then return end
 
-local MAJOR, MINOR = "LibClassicDurations", 22
+local MAJOR, MINOR = "LibClassicDurations", 24
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
@@ -451,7 +451,7 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(event)
 
     if indirectRefreshSpells[spellName] then
         local refreshTable = indirectRefreshSpells[spellName]
-        if refreshTable.event == eventType then
+        if refreshTable.events[eventType] then
             local targetSpellID = refreshTable.targetSpellID
 
             local condition = refreshTable.condition
@@ -461,7 +461,6 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(event)
             end
 
             RefreshTimer(srcGUID, dstGUID, targetSpellID)
-            return
         end
     end
 
@@ -679,6 +678,7 @@ local function GetGUIDAuraTime(dstGUID, spellName, spellID, srcGUID, isStacking)
             if not applicationTable then return end
             local durationFunc, startTime, auraType, comboPoints = unpack(applicationTable)
             local duration = cleanDuration(durationFunc, spellID, srcGUID, comboPoints)
+            if not duration then return nil end
             local mul = getDRMul(dstGUID, spellID)
             -- local mul = getDRMul(dstGUID, lastRankID)
             duration = duration * mul
