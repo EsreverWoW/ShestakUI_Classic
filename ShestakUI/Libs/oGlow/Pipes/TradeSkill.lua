446c73d908fcb684eaa0352fce883698d3a33356
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-09-26 13:49:40 -0500

    Fix oGlow tradeskill pipe

--- ShestakUI/Libs/oGlow/Pipes/TradeSkill.lua
+++ ShestakUI/Libs/oGlow/Pipes/TradeSkill.lua
@@ -1,19 +1,38 @@
 local _E
 local hook
 
-local pipe = function(_, id)
-	if not id then return end
-	local itemLink = C_TradeSkillUI.GetRecipeItemLink(id)
+local pipe
+if oGlow:IsClassic() then
+	pipe = function(id)
+		if not id then return end
+		local itemLink = GetTradeSkillItemLink(id)
 
-	if itemLink then
-		oGlow:CallFilters("tradeskill", TradeSkillFrame.DetailsFrame.Contents.ResultIcon, _E and itemLink)
+		if itemLink then
+			oGlow:CallFilters("tradeskill", TradeSkillSkillIcon, _E and itemLink)
+		end
+
+		for i = 1, GetTradeSkillNumReagents(id) do
+			local reagentFrame = _G["TradeSkillReagent"..i.."IconTexture"]
+			local reagentLink = GetTradeSkillReagentItemLink(id, i)
+
+			oGlow:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
+		end
 	end
+else
+	pipe = function(_, id)
+		if not id then return end
+		local itemLink = C_TradeSkillUI.GetRecipeItemLink(id)
 
-	for i = 1, C_TradeSkillUI.GetRecipeNumReagents(id) do
-		local reagentFrame = TradeSkillFrame.DetailsFrame.Contents.Reagents[i].Icon
-		local reagentLink = C_TradeSkillUI.GetRecipeReagentItemLink(id, i)
+		if itemLink then
+			oGlow:CallFilters("tradeskill", TradeSkillFrame.DetailsFrame.Contents.ResultIcon, _E and itemLink)
+		end
 
-		oGlow:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
+		for i = 1, C_TradeSkillUI.GetRecipeNumReagents(id) do
+			local reagentFrame = TradeSkillFrame.DetailsFrame.Contents.Reagents[i].Icon
+			local reagentLink = C_TradeSkillUI.GetRecipeReagentItemLink(id, i)
+
+			oGlow:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
+		end
 	end
 end
 
@@ -23,7 +42,9 @@ local doHook = function()
 			if _E then return pipe(...) end
 		end
 
-		if not oGlow:IsClassic() then
+		if oGlow:IsClassic() then
+			hooksecurefunc("TradeSkillFrame_SetSelection", hook)
+		else
 			hooksecurefunc(TradeSkillFrame.RecipeList, "SetSelectedRecipeID", hook)
 		end
 	end
