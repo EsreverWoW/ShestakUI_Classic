ae8d21cecc23161453491a435bc02303a8fee6b5
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-10-05 17:23:15 -0500

    Add charges to Temporary Weapon Enchants

--- ShestakUI/Modules/Auras/BuffFrame.lua
+++ ShestakUI/Modules/Auras/BuffFrame.lua
@@ -4,7 +4,6 @@ if C.aura.player_auras ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Style player buff(by Tukz)
 ----------------------------------------------------------------------------------------
-_G.BUFF_WARNING_TIME = 0
 local rowbuffs = 16
 
 local GetFormattedTime = function(s)
@@ -27,6 +26,16 @@ TemporaryEnchantFrame:SetPoint("TOPRIGHT", BuffsAnchor, "TOPRIGHT", 0, 0)
 
 _G["TempEnchant2"]:ClearAllPoints()
 _G["TempEnchant2"]:SetPoint("RIGHT", _G["TempEnchant1"], "LEFT", -3, 0)
+
+for enchantIndex = 1, 2 do
+	local enchantButton = _G["TempEnchant" .. enchantIndex]
+	local charges = enchantButton:CreateFontString(enchantButton:GetName() .. "Charges", "ARTWORK", "NumberFontNormal")
+	charges:ClearAllPoints()
+	charges:SetPoint("BOTTOMLEFT", 2, 0)
+	charges:SetFont(C.font.auras_font, C.font.auras_font_size, C.font.auras_font_style)
+	charges:SetShadowOffset(C.font.auras_font_shadow and 1 or 0, C.font.auras_font_shadow and -1 or 0)
+	charges:Hide()
+end
 
 local function StyleBuffs(buttonName, index)
 	local buff = _G[buttonName..index]
@@ -87,27 +96,6 @@ local function StyleBuffs(buttonName, index)
 	end
 end
 
-local function UpdateDuration(auraButton, timeLeft)
-	local name = auraButton:GetName()
-	if not strmatch(name, "TempEnchant") then return end
-
-	local index = strmatch(name:sub(-1), "2") and 17 or 16
-	local quality = GetInventoryItemQuality("player", index)
-
-	if quality then
-		auraButton:SetBackdropBorderColor(GetItemQualityColor(quality))
-	end
-
-	local duration = auraButton.duration
-	if timeLeft and C.aura.show_timer == true then
-		duration:SetFormattedText(GetFormattedTime(timeLeft))
-		duration:SetVertexColor(1, 1, 1)
-		duration:Show()
-	else
-		duration:Hide()
-	end
-end
-
 local function UpdateBuffAnchors()
 	local buttonName = "BuffButton"
 	local previousBuff, aboveBuff
@@ -154,8 +142,62 @@ local function UpdateDebuffAnchors(buttonName, index)
 	_G[buttonName..index]:Hide()
 end
 
-AuraButton_UpdateDuration = function() end
-
 hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", UpdateBuffAnchors)
 hooksecurefunc("DebuffButton_UpdateAnchors", UpdateDebuffAnchors)
-hooksecurefunc("AuraButton_UpdateDuration", UpdateDuration)
+
+function AuraButton_UpdateDuration(buff, timeLeft)
+	local name = buff:GetName()
+	if not strmatch(name, "TempEnchant") or name == "TempEnchant3" then return end
+
+	local hasMainHandEnchant, _, mainHandCharges, _, hasOffHandEnchant, _, offHandCharges = GetWeaponEnchantInfo()
+	local index = 1
+
+	if hasOffHandEnchant then
+		local charges = _G["TempEnchant" .. index .. "Charges"]
+		charges:SetText(offHandCharges)
+
+		if offHandCharges > 0 then
+			charges:Show()
+		else
+			charges:Hide()
+		end
+
+		local quality = GetInventoryItemQuality("player", 17)
+
+		if quality then
+			buff:SetBackdropBorderColor(GetItemQualityColor(quality))
+		end
+
+		index = index + 1
+	end
+
+	if hasMainHandEnchant then
+		local charges = _G["TempEnchant" .. index .. "Charges"]
+		charges:SetText(mainHandCharges)
+
+		if mainHandCharges > 0 then
+			charges:Show()
+		else
+			charges:Hide()
+		end
+
+		local quality = GetInventoryItemQuality("player", 16)
+
+		if quality then
+			buff:SetBackdropBorderColor(GetItemQualityColor(quality))
+		end
+
+		index = index + 1
+	end
+
+	local duration = buff.duration
+	if timeLeft and C.aura.show_timer == true then
+		duration:SetFormattedText(GetFormattedTime(timeLeft))
+		duration:SetVertexColor(1, 1, 1)
+		duration:Show()
+	else
+		duration:Hide()
+	end
+end
+
+BuffFrame:SetScript("OnUpdate", nil) -- Disable BuffFrame_OnUpdate that change alpha
