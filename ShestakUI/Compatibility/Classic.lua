45856de16fc26526ced7393cbed6fc3e23041774
Dirk-Jan Wassink <DJWassink@users.noreply.github.com>
2020-03-26 17:39:12 +0100

    Fix shaman color string

--- ShestakUI/Compatibility/Classic.lua
+++ ShestakUI/Compatibility/Classic.lua
@@ -34,7 +34,7 @@ LibClassicDurations:Register("ShestakUI")
 RAID_CLASS_COLORS.SHAMAN.r = 0
 RAID_CLASS_COLORS.SHAMAN.g = 0.44
 RAID_CLASS_COLORS.SHAMAN.b = 0.87
-RAID_CLASS_COLORS.SHAMAN.colorStr = "0070de"
+RAID_CLASS_COLORS.SHAMAN.colorStr = "ff0070de"
 
 ----------------------------------------------------------------------------------------
 --	Specialization Functions
