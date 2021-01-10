4bf597771bca020165188d71c36d17e17f274754
Wetxius <wetxius@gmail.com>
2021-01-10 13:57:07 +0300

    Enable AtlasLoot skin.

--- ShestakUI/Modules/Skins/AtlasLoot.lua
+++ ShestakUI/Modules/Skins/AtlasLoot.lua
@@ -37,7 +37,7 @@ frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", function(self)
 	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
 	if IsAddOnLoaded("Skinner") or IsAddOnLoaded("Aurora") then return end
-	if not IsAddOnLoaded("AtlasLoot") then return end
+	if not IsAddOnLoaded("AtlasLootClassic") then return end
 
 	local AtlasLootFrame = _G["AtlasLoot_GUI-Frame"]
 	local AtlasLootItemFrame = _G["AtlasLoot_GUI-ItemFrame"]
