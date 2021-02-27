a88868a2c21c53b98afb78feef8c3b0b319c7067
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-02-27 15:37:10 -0600

    Disable Mount Source in Classic

--- ShestakUI/Modules/Tooltip/MountSource.lua
+++ ShestakUI/Modules/Tooltip/MountSource.lua
@@ -1,5 +1,5 @@
 ﻿local T, C, L, _ = unpack(select(2, ...))
-if C.tooltip.enable ~= true or C.tooltip.mount ~= true then return end
+if T.classic or C.tooltip.enable ~= true or C.tooltip.mount ~= true then return end
 
 ----------------------------------------------------------------------------------------
 --	Show source of mount
