689304dc3dcf90d909111319b17ee74b48563a44
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-04-06 22:52:45 -0500

    BCC Drinking.lua spellIDs

--- ShestakUI/Modules/Announcements/Drinking.lua
+++ ShestakUI/Modules/Announcements/Drinking.lua
@@ -4,11 +4,36 @@ if C.announcements.drinking ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Announce enemy drinking in arena(by Duffed)
 ----------------------------------------------------------------------------------------
-local drinkSpell = {
-	[GetSpellInfo(118358)] = true,	-- Drink
-	[GetSpellInfo(167152)] = true,	-- Refreshment
-	[GetSpellInfo(167268)] = true,	-- Ba'ruun's Bountiful Bloom
-}
+local drinkSpell = {}
+if T.classic then
+	drinkSpell = {
+		[GetSpellInfo(18071)] = true,	-- Enriched Manna Biscuit
+		[GetSpellInfo(18140)] = true,	-- Blessed Sunfruit Juice
+		[GetSpellInfo(23541)] = true,	-- Warsong Gulch Iron Ration
+		[GetSpellInfo(23542)] = true,	-- Warsong Gulch Field Ration
+		[GetSpellInfo(23692)] = true,	-- Alterac Manna Biscuit
+		[GetSpellInfo(24409)] = true,	-- Arathi Basin Field Ration
+		[GetSpellInfo(24410)] = true,	-- Arathi Basin Iron Ration
+		[GetSpellInfo(24384)] = true,	-- Essence Mango
+		[GetSpellInfo(25990)] = true,	-- Graccu's Mince Meat Fruitcake
+		[GetSpellInfo(27089)] = true,	-- Drink
+		[GetSpellInfo(29055)] = true,	-- Refreshing Red Apple
+		[GetSpellInfo(33772)] = true,	-- Underspore Pod
+		[GetSpellInfo(41031)] = true,	-- Enriched Terocone Juice
+		[GetSpellInfo(42309)] = true,	-- Brain Food
+		[GetSpellInfo(44115)] = true,	-- Brewfest Drink
+		[GetSpellInfo(44166)] = true,	-- Refreshment
+		[GetSpellInfo(45020)] = true,	-- Holiday Drink
+		[GetSpellInfo(49472)] = true,	-- Drink Coffee
+	}
+else
+	drinkSpell = {
+		[GetSpellInfo(118358)] = true,	-- Drink
+		[GetSpellInfo(167152)] = true,	-- Refreshment
+		[GetSpellInfo(167268)] = true,	-- Ba'ruun's Bountiful Bloom
+	}
+end
+
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
