11f5f94b4c54f9533614160dbc3dd9b74fa1a765
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-04-13 10:35:12 -0500

    Socket Skin: LoadSkin and comment

--- ShestakUI/Compatibility/BCC/Modules/Skins/Socket.lua
+++ ShestakUI/Compatibility/BCC/Modules/Skins/Socket.lua
@@ -4,5 +4,8 @@ if not T.BCC or C.skins.blizzard_frames ~= true then return end
 ----------------------------------------------------------------------------------------
 --	Socket skin
 ----------------------------------------------------------------------------------------
+local function LoadSkin()
+    -- /run LoadAddOn("Blizzard_ItemSocketingUI");ItemSocketingFrame:Show()
+end
 
 T.SkinFuncs["Blizzard_ItemSocketingUI"] = LoadSkin
