4d3146008613128c01d354a34be91a4f4efa5384
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-04-23 23:42:53 -0500

    Again, disable DropDownMenu skinning in BCC for now

--- ShestakUI/Compatibility/Classic/Modules/Skins/Blizzard/Others.lua
+++ ShestakUI/Compatibility/Classic/Modules/Skins/Blizzard/Others.lua
@@ -104,17 +104,14 @@ SkinBlizzUI:SetScript("OnEvent", function(self, event, addon)
 		_G["PetBattleQueueReadyFrame"].DeclineButton:SkinButton()
 
 		-- Reskin Dropdown menu
-		hooksecurefunc("UIDropDownMenu_InitializeHelper", function(frame)
-			for i = 1, UIDROPDOWNMENU_MAXLEVELS do
-				if T.BCC then
-					_G["DropDownList"..i.."Backdrop"]:SetTemplate("Transparent", nil, true)
-					_G["DropDownList"..i.."MenuBackdrop"]:SetTemplate("Transparent", nil, true)
-				else
+		if not T.BCC then
+			hooksecurefunc("UIDropDownMenu_InitializeHelper", function(frame)
+				for i = 1, UIDROPDOWNMENU_MAXLEVELS do
 					_G["DropDownList"..i.."Backdrop"]:SetTemplate("Transparent")
 					_G["DropDownList"..i.."MenuBackdrop"]:SetTemplate("Transparent")
 				end
-			end
-		end)
+			end)
+		end
 
 		-- Reskin menu
 		local ChatMenus = {
