735a3224a13e07cdf5c5320d537e7898f1dae41c
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-05-16 15:47:44 -0500

    [Automation] AutoDismount: Also take care of Shapeshift Forms when OOC

--- ShestakUI/Modules/Automation/AutoDismount.lua
+++ ShestakUI/Modules/Automation/AutoDismount.lua
@@ -19,13 +19,42 @@ local sittingMessages = {
 	[SPELL_FAILED_NOT_STANDING]					= true,
 }
 
+local shapeshiftMessages = {
+	[ERR_CANT_INTERACT_SHAPESHIFTED]			= true,
+	[ERR_MOUNT_SHAPESHIFTED]					= true,
+	[ERR_NOT_WHILE_SHAPESHIFTED]				= true,
+	[ERR_NO_ITEMS_WHILE_SHAPESHIFTED]			= true,
+	[ERR_SHAPESHIFT_FORM_CANNOT_EQUIP]			= true,
+	[ERR_TAXIPLAYERSHAPESHIFTED]				= true,
+	[SPELL_FAILED_NOT_SHAPESHIFT]				= true,
+	[SPELL_FAILED_NO_ITEMS_WHILE_SHAPESHIFTED]	= true,
+	[SPELL_NOT_SHAPESHIFTED]					= true,
+	[SPELL_NOT_SHAPESHIFTED_NOSPACE]			= true,
+}
+
+local shapeshiftSpells = {
+	[GetSpellInfo(5487)]		= true, -- Bear Form
+	[GetSpellInfo(9634)]		= true, -- Dire Bear Form
+	[GetSpellInfo(768)]		= true, -- Cat Form
+	[GetSpellInfo(783)]		= true, -- Travel Form
+	[GetSpellInfo(1066)]		= true, -- Aquatic Form
+	[GetSpellInfo(24858)]		= true, -- Moonkin Form
+	[GetSpellInfo(2645)]		= true, -- Ghost Wolf
+}
+
 local function CheckErrorMessage(_, _, _, msg)
-	if mountedMessages[msg] then
-		if not IsMounted() then return end
+	if mountedMessages[msg] and IsMounted() then
 		Dismount()
 		UIErrorsFrame:Clear()
 	elseif sittingMessages[msg] then
 		DoEmote("stand")
+		UIErrorsFrame:Clear()
+	elseif shapeshiftMessages[msg] then
+		for k, _ in pairs(shapeshiftSpells) do
+			if not InCombatLockdown() then
+				CancelSpellByName(k)
+			end
+		end
 		UIErrorsFrame:Clear()
 	end
 end
