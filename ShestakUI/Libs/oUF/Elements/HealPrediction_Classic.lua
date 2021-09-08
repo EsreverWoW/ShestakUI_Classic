41ecf185c32da59996d65d339a56432d2f7779d0
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-09-08 14:08:55 -0500

    [oUF] HealPrediction_Classic: Also obtain data for "bomb" heals like Lifebloom's expiration

--- ShestakUI/Libs/oUF/Elements/HealPrediction_Classic.lua
+++ ShestakUI/Libs/oUF/Elements/HealPrediction_Classic.lua
@@ -14,7 +14,7 @@ local function Update(self, event, unit)
 	if(hp.PreUpdate) then hp:PreUpdate(unit) end
 
 	local GUID = UnitGUID(unit)
-	local OverTimeHeals = (HealComm:GetHealAmount(GUID, HealComm.OVERTIME_HEALS) or 0) * (HealComm:GetHealModifier(GUID) or 1)
+	local OverTimeHeals = (HealComm:GetHealAmount(GUID, HealComm.OVERTIME_AND_BOMB_HEALS) or 0) * (HealComm:GetHealModifier(GUID) or 1)
 	local DirectHeals = UnitGetIncomingHeals(unit) or 0
 	local IncomingHeals = DirectHeals >= DirectHeals + OverTimeHeals and DirectHeals or DirectHeals + OverTimeHeals
 	local Health = UnitHealth(unit)
