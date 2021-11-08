8799d775864c414f129761259f2eee3cfa4b644d
EsreverWoW <EsreverWoW@users.noreply.github.com>
2021-11-08 12:00:13 -0600

    Fixed minimap icons. pt. 2

--- ShestakUI/Modules/Maps/ButtonCollect.lua
+++ ShestakUI/Modules/Maps/ButtonCollect.lua
@@ -39,9 +39,7 @@ local function SkinButton(f)
 				region:SetPoint("TOPLEFT", f, "TOPLEFT", 2, -2)
 				region:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -2, 2)
 				region:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-				if not T.classic then
-					region:SetDrawLayer("ARTWORK") -- FIXME: Black Overlay and/or Border Texture in Classic/BCC
-				end
+				region:SetDrawLayer("ARTWORK")
 				if f:GetName() == "PS_MinimapButton" then
 					region.SetPoint = T.dummy
 				end
