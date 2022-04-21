2ca6ec215c0d84dd25f8656785f90bbef22d1b52
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2022-04-21 17:46:35 -0500

    [Announcements] Drinking: Add more spellIDs for Wrath Classic

--- ShestakUI/Modules/Announcements/Drinking.lua
+++ ShestakUI/Modules/Announcements/Drinking.lua
@@ -5,7 +5,7 @@ if C.announcements.drinking ~= true then return end
 --	Announce enemy drinking in arena(by Duffed)
 ----------------------------------------------------------------------------------------
 local drinkSpell = {}
-if T.BCC then
+if T.classic then
 	drinkSpell = {
 		[GetSpellInfo(18071)] = true,	-- Enriched Manna Biscuit
 		[GetSpellInfo(18140)] = true,	-- Blessed Sunfruit Juice
@@ -18,6 +18,7 @@ if T.BCC then
 		[GetSpellInfo(25990)] = true,	-- Graccu's Mince Meat Fruitcake
 		[GetSpellInfo(27089)] = true,	-- Drink
 		[GetSpellInfo(29055)] = true,	-- Refreshing Red Apple
+		[GetSpellInfo(33004)] = true,	-- Clamlette Magnifique
 		[GetSpellInfo(33772)] = true,	-- Underspore Pod
 		[GetSpellInfo(41031)] = true,	-- Enriched Terocone Juice
 		[GetSpellInfo(42309)] = true,	-- Brain Food
@@ -25,6 +26,12 @@ if T.BCC then
 		[GetSpellInfo(44166)] = true,	-- Refreshment
 		[GetSpellInfo(45020)] = true,	-- Holiday Drink
 		[GetSpellInfo(49472)] = true,	-- Drink Coffee
+		[GetSpellInfo(57301)] = true,	-- Great Feast
+		[GetSpellInfo(57426)] = true,	-- Fish Feast
+		[GetSpellInfo(58474)] = true,	-- Small Feast
+		[GetSpellInfo(58465)] = true,	-- Gigantic Feast
+		[GetSpellInfo(65422)] = true,	-- Food
+		[GetSpellInfo(66476)] = true,	-- Bountiful Feast
 	}
 else
 	drinkSpell = {
