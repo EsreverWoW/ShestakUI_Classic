95b2f147e83f3f9d7f21e62eaf2d75385ae910a7
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2022-12-07 15:33:43 -0600

    Fixed incoming heal check.

--- ShestakUI/Modules/Blizzard/CombatText.lua
+++ ShestakUI/Modules/Blizzard/CombatText.lua
@@ -73,7 +73,7 @@ local r, g, b, lowMana, lowHealth
 local function OnEvent(_, event, subevent, powerType)
 	if event == "COMBAT_TEXT_UPDATE" then
 		local arg2, arg3 = GetCurrentCombatTextEventInfo()
-		if SHOW_COMBAT_TEXT == "0" then
+		if (T.Classic and SHOW_COMBAT_TEXT == "0") or (T.Mainline and not CVarCallbackRegistry:GetCVarValueBool("enableFloatingCombatText")) then
 			return
 		else
 			if subevent == "DAMAGE" then
