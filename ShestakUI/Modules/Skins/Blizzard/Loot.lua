422372515b3dbe05dfd47af2ada036b1ebe758c4
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2022-12-07 23:35:06 -0600

    Enable BonusRoll and Loot skins.

--- ShestakUI/Modules/Skins/Blizzard/Loot.lua
+++ ShestakUI/Modules/Skins/Blizzard/Loot.lua
@@ -86,35 +86,75 @@ local function LoadSkin()
 	-- Loot frame
 	if C.loot.lootframe == true or (IsAddOnLoaded("AdiBags") or IsAddOnLoaded("ArkInventory") or IsAddOnLoaded("cargBags_Nivaya") or IsAddOnLoaded("cargBags") or IsAddOnLoaded("Bagnon") or IsAddOnLoaded("Combuctor") or IsAddOnLoaded("TBag") or IsAddOnLoaded("BaudBag")) then return end
 
-	LootFrame:StripTextures(true)
-	LootFrameInset:StripTextures()
-	LootFrame:SetTemplate("Transparent")
+	if T.Classic then
+		LootFrame:StripTextures()
+		LootFrame:SetTemplate("Transparent")
+		LootFrameInset:StripTextures()
+		LootFramePortraitOverlay:Kill()
 
-	T.SkinNextPrevButton(LootFrameDownButton)
-	T.SkinNextPrevButton(LootFrameUpButton, true)
+		T.SkinNextPrevButton(LootFrameDownButton)
+		T.SkinNextPrevButton(LootFrameUpButton, true)
 
-	T.SkinCloseButton(LootFrameCloseButton)
+		T.SkinCloseButton(LootFrameCloseButton)
 
-	for i = 1, LOOTFRAME_NUMBUTTONS do
-		local slot = _G["LootButton"..i]
-		local icon = _G["LootButton"..i.."IconTexture"]
-		local name = _G["LootButton"..i.."NameFrame"]
-		_G["LootButton"..i.."IconQuestTexture"]:SetAlpha(0)
+		for i = 1, LOOTFRAME_NUMBUTTONS do
+			local slot = _G["LootButton"..i]
+			local icon = _G["LootButton"..i.."IconTexture"]
+			local name = _G["LootButton"..i.."NameFrame"]
 
-		slot:StyleButton()
-		slot:SetNormalTexture(0)
-		slot:SetTemplate("Default")
+			slot:StyleButton()
+			slot:SetNormalTexture(0)
+			slot:SetTemplate("Default")
 
-		icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-		icon:ClearAllPoints()
-		icon:SetPoint("TOPLEFT", 2, -2)
-		icon:SetPoint("BOTTOMRIGHT", -2, 2)
+			icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			icon:ClearAllPoints()
+			icon:SetPoint("TOPLEFT", 2, -2)
+			icon:SetPoint("BOTTOMRIGHT", -2, 2)
 
-		name:Hide()
-		name.border = CreateFrame("Frame", nil, slot)
-		name.border:CreateBackdrop("Overlay")
-		name.border.backdrop:SetPoint("TOPLEFT", name, 11, -13)
-		name.border.backdrop:SetPoint("BOTTOMRIGHT", name, -8, 12)
+			name:Hide()
+			name.border = CreateFrame("Frame", nil, slot)
+			name.border:CreateBackdrop("Overlay")
+			name.border.backdrop:SetPoint("TOPLEFT", name, 11, -13)
+			name.border.backdrop:SetPoint("BOTTOMRIGHT", name, -8, 12)
+		end
+	else
+		LootFrame:StripTextures(true)
+		LootFrame:SetTemplate("Transparent")
+
+		T.SkinCloseButton(LootFrame.ClosePanelButton)
+
+		hooksecurefunc(LootFrame.ScrollBox, "Update", function(frame)
+			for _, button in next, {frame.ScrollTarget:GetChildren()} do
+				local item = button.Item
+				if item and not item.styled then
+					item:StyleButton()
+					item:SetNormalTexture(0)
+					item:SetTemplate("Default")
+
+					item.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+					item.icon:ClearAllPoints()
+					item.icon:SetPoint("TOPLEFT", 2, -2)
+					item.icon:SetPoint("BOTTOMRIGHT", -2, 2)
+
+					button:CreateBackdrop("Overlay")
+					button.backdrop:SetPoint("TOPLEFT", 0, 0)
+					button.backdrop:SetPoint("BOTTOMRIGHT", 0, 0)
+
+					button.HighlightNameFrame:SetAlpha(0)
+					button.PushedNameFrame:SetAlpha(0)
+					item.IconBorder:SetAlpha(0)
+					button.NameFrame:Hide()
+
+					item.styled = true
+				end
+
+				button.IconQuestTexture:SetAlpha(0)
+				button.BorderFrame:SetAlpha(0)
+				if button.QualityStripe then
+					button.QualityStripe:SetAlpha(0)
+				end
+			end
+		end)
 	end
 end
 
