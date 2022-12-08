97b00870bda70b9aacf0ac795f5241d57aa6f312
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2022-12-07 23:41:48 -0600

    Fixed oGlow Loot.

--- ShestakUI/Libs/oGlow/Pipes/Loot.lua
+++ ShestakUI/Libs/oGlow/Pipes/Loot.lua
@@ -4,19 +4,31 @@ if C.loot.lootframe == true then return end
 local hook
 local _E
 
-local update = function()
-	if LootFrame:IsShown() and oGlow:IsPipeEnabled"loot" then
-		for i = 1, LOOTFRAME_NUMBUTTONS or 4 do
-			local slotFrame = _G["LootButton"..i]
-			local slot = slotFrame.slot
+local update = function(self)
+	if oGlow:IsClassic() then
+		if LootFrame:IsShown() and oGlow:IsPipeEnabled("loot") then
+			for i = 1, LOOTFRAME_NUMBUTTONS or 4 do
+				local slotFrame = _G["LootButton"..i]
+				local slot = slotFrame.slot
 
-			local itemLink
-			if slot then
-				itemLink = GetLootSlotLink(slot)
+				local itemLink
+				if slot then
+					itemLink = GetLootSlotLink(slot)
+				end
+
+				oGlow:CallFilters("loot", slotFrame, _E and itemLink)
 			end
-
-			oGlow:CallFilters("loot", slotFrame, _E and itemLink)
 		end
+	else
+		local slot = self:GetSlotIndex()
+		local slotFrame = self.Item
+
+		local itemLink
+		if slot then
+			itemLink = GetLootSlotLink(slot)
+		end
+
+		oGlow:CallFilters("loot", slotFrame, _E and itemLink)
 	end
 end
 
@@ -24,23 +36,31 @@ local enable = function(self)
 	_E = true
 
 	if not hook then
-		LootFrameUpButton:HookScript("OnClick", update)
-		LootFrameDownButton:HookScript("OnClick", update)
+		if oGlow:IsClassic() then
+			LootFrameUpButton:HookScript("OnClick", update)
+			LootFrameDownButton:HookScript("OnClick", update)
+		else
+			hooksecurefunc(LootFrameElementMixin, "Init", update)
+		end
 
 		hook = true
 	end
 
-	self:RegisterEvent("LOOT_OPENED", update)
-	self:RegisterEvent("LOOT_SLOT_CLEARED", update)
-	self:RegisterEvent("LOOT_SLOT_CHANGED", update)
+	if oGlow:IsClassic() then
+		self:RegisterEvent("LOOT_OPENED", update)
+		self:RegisterEvent("LOOT_SLOT_CLEARED", update)
+		self:RegisterEvent("LOOT_SLOT_CHANGED", update)
+	end
 end
 
 local disable = function(self)
 	_E = nil
 
-	self:UnregisterEvent("LOOT_OPENED", update)
-	self:UnregisterEvent("LOOT_SLOT_CLEARED", update)
-	self:UnregisterEvent("LOOT_SLOT_CHANGED", update)
+	if oGlow:IsClassic() then
+		self:UnregisterEvent("LOOT_OPENED", update)
+		self:UnregisterEvent("LOOT_SLOT_CLEARED", update)
+		self:UnregisterEvent("LOOT_SLOT_CHANGED", update)
+	end
 end
 
 oGlow:RegisterPipe("loot", enable, disable, update, "Loot frame")
