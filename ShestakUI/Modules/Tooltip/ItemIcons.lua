6abb6cbec22a278ffca70ba4f456460c7e57618f
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2022-12-08 00:53:13 -0600

    Fixed ItemIcons.

--- ShestakUI/Modules/Tooltip/ItemIcons.lua
+++ ShestakUI/Modules/Tooltip/ItemIcons.lua
@@ -1,6 +1,6 @@
 local T, C, L, _ = unpack(select(2, ...))
 if C.tooltip.enable ~= true or C.tooltip.item_icon ~= true then return end
-if T.newPatch then return end -- BETA
+
 ----------------------------------------------------------------------------------------
 --	Adds item icons to tooltips(Tipachu by Tuller)
 ----------------------------------------------------------------------------------------
@@ -11,38 +11,70 @@ local function setTooltipIcon(self, icon)
 	end
 end
 
-local function newTooltipHooker(method, func)
-	return function(tooltip)
-		local modified = false
+local whiteTooltip = {
+	[GameTooltip] = true,
+	[ItemRefTooltip] = true,
+	[ItemRefShoppingTooltip1] = true,
+	[ItemRefShoppingTooltip2] = true,
+	[ShoppingTooltip1] = true,
+	[ShoppingTooltip2] = true,
+}
 
-		tooltip:HookScript("OnTooltipCleared", function()
-			modified = false
-		end)
+if T.Classic then
+	local function newTooltipHooker(method, func)
+		return function(tooltip)
+			local modified = false
 
-		tooltip:HookScript(method, function(self, ...)
-			if not modified then
-				modified = true
-				func(self, ...)
+			tooltip:HookScript("OnTooltipCleared", function()
+				modified = false
+			end)
+
+			tooltip:HookScript(method, function(self, ...)
+				if not modified then
+					modified = true
+					func(self, ...)
+				end
+			end)
+		end
+	end
+
+	local hookItem = newTooltipHooker("OnTooltipSetItem", function(self)
+		local _, link = self:GetItem()
+		if link then
+			setTooltipIcon(self, GetItemIcon(link))
+		end
+	end)
+
+	local hookSpell = newTooltipHooker("OnTooltipSetSpell", function(self)
+		local _, id = self:GetSpell()
+		if id then
+			setTooltipIcon(self, select(3, GetSpellInfo(id)))
+		end
+	end)
+
+	for tooltip in pairs(whiteTooltip) do
+		hookItem(tooltip)
+		hookSpell(tooltip)
+	end
+else
+	local function hookItem(self)
+		if whiteTooltip[self] then
+			local _, link = TooltipUtil.GetDisplayedItem(self)
+			if link then
+				setTooltipIcon(self, GetItemIcon(link))
 			end
-		end)
+		end
 	end
-end
 
-local hookItem = newTooltipHooker("OnTooltipSetItem", function(self)
-	local _, link = self:GetItem()
-	if link then
-		setTooltipIcon(self, GetItemIcon(link))
+	local function hookSpell(self)
+		if whiteTooltip[self] then
+			local _, id = self:GetSpell()
+			if id then
+				setTooltipIcon(self, select(3, GetSpellInfo(id)))
+			end
+		end
 	end
-end)
 
-local hookSpell = newTooltipHooker("OnTooltipSetSpell", function(self)
-	local _, id = self:GetSpell()
-	if id then
-		setTooltipIcon(self, select(3, GetSpellInfo(id)))
-	end
-end)
-
-for _, tooltip in pairs{GameTooltip, ItemRefTooltip, ItemRefShoppingTooltip1, ItemRefShoppingTooltip2, ShoppingTooltip1, ShoppingTooltip2} do
-	hookItem(tooltip)
-	hookSpell(tooltip)
+	TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, hookItem)
+	TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Spell, hookSpell)
 end
