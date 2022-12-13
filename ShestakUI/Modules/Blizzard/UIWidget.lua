19a8ac5f3bc3ffd636483ea537451777699b7532
Wetxius <Wetxius@users.noreply.github.com>
2022-12-13 17:35:36 +0300

    [UIWidget] Try to fix wrong max bar for Vigor.
    # Conflicts:
    #	ShestakUI/Modules/Blizzard/UIWidget.lua

--- ShestakUI/Modules/Blizzard/UIWidget.lua
+++ ShestakUI/Modules/Blizzard/UIWidget.lua
@@ -238,7 +238,9 @@ if T.Mainline then
 			VigorBar[i]:SetValue(value)
 		end
 
-		if total ~= 6 then
+		if total < 6 and IsPlayerSpell(377922) then total = 6 end -- sometimes it return 5
+
+		if total < 6 then
 			for i = total + 1, 6 do
 				VigorBar[i]:Hide()
 				VigorBar[i]:SetValue(0)
