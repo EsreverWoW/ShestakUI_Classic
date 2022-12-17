ef40d2de1b83d0ba5192d54d4790e5dc56a9b9e2
Wetxius <Wetxius@users.noreply.github.com>
2022-12-17 17:16:09 +0300

    [Movers] Added check for Bar7Holder and Bar8Holder.
    # Conflicts:
    #	ShestakUI/Core/Movers.lua

--- ShestakUI/Core/Movers.lua
+++ ShestakUI/Core/Movers.lua
@@ -40,10 +40,16 @@ T.MoverFrames = {
 	T_DE_BUFF_BAR_Anchor,
 	SplitBarLeft,
 	SplitBarRight,
-	Bar7Holder,
-	Bar8Holder,
 	UIWidgetPowerBarAnchor
 }
+
+if C.actionbar.bar7_enable then
+	tinsert(T.MoverFrames, Bar7Holder)
+end
+
+if C.actionbar.bar8_enable then
+	tinsert(T.MoverFrames, Bar8Holder)
+end
 
 local unitFrames = {
 	oUF_Player,
@@ -84,7 +90,9 @@ if C.actionbar.editor then
 	tinsert(T.MoverFrames, Bar3Holder)
 	tinsert(T.MoverFrames, Bar4Holder)
 	tinsert(T.MoverFrames, Bar5Holder)
-	tinsert(T.MoverFrames, Bar6Holder)
+	if T.Mainline then
+		tinsert(T.MoverFrames, Bar6Holder)
+	end
 	tremove(T.MoverFrames, 5)	-- RightActionBarAnchor
 	tremove(T.MoverFrames, 4)	-- ActionBarAnchor
 end
