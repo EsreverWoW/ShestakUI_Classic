06f96a7f16b9462fcffecfeacaca273275129873
Wetxius <Wetxius@users.noreply.github.com>
2022-12-17 17:14:26 +0300

    Cleanup.

--- ShestakUI/Modules/ActionBars/Bar5.lua
+++ ShestakUI/Modules/ActionBars/Bar5.lua
@@ -33,7 +33,6 @@ bar:SetFrameStrata("LOW")
 
 bar:RegisterEvent("PLAYER_ENTERING_WORLD")
 bar:SetScript("OnEvent", function(self, event)
-	-- self:UnregisterAllEvents()
 	local NumPerRows = C.actionbar.bar5_row
 	local NextRowButtonAnchor = _G["MultiBarBottomRightButton1"]
 	for i = 1, 12 do
@@ -84,7 +83,6 @@ bar:SetScript("OnEvent", function(self, event)
 				end
 			end
 		end
-		-- b.SetPoint = T.dummy -- Prevent moving by EditMode
 	end
 end)
 
