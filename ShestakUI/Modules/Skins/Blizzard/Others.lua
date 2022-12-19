d71a1b80a444e1429451796df199a0df55e953e1
Wetxius <Wetxius@users.noreply.github.com>
2022-12-19 13:40:21 +0300

    [Skins] Added skin for custom dropdown frames.
    # Conflicts:
    #	ShestakUI/Modules/Skins/Blizzard/Others.lua

--- ShestakUI/Modules/Skins/Blizzard/Others.lua
+++ ShestakUI/Modules/Skins/Blizzard/Others.lua
@@ -145,18 +145,16 @@ SkinBlizzUI:SetScript("OnEvent", function(_, _, addon)
 		_G["PetBattleQueueReadyFrame"].DeclineButton:SkinButton()
 
 		-- Reskin Dropdown menu
+		local dropdowns = {"DropDownList", "L_DropDownList", "Lib_DropDownList"}
 		hooksecurefunc("UIDropDownMenu_InitializeHelper", function()
-			for i = 1, UIDROPDOWNMENU_MAXLEVELS do
-				if _G["DropDownList"..i.."MenuBackdrop"].NineSlice then
-					_G["DropDownList"..i.."MenuBackdrop"].NineSlice:SetAlpha(0)
+			for _, name in next, dropdowns do
+				for i = 1, UIDROPDOWNMENU_MAXLEVELS do
+					local backdrop = _G[name..i.."Backdrop"]
+					if backdrop then
+						backdrop:SetTemplate("Transparent")
+						_G[name..i.."MenuBackdrop"].NineSlice:SetTemplate("Transparent")
+					end
 				end
-
-				if T.Mainline then
-					_G["DropDownList"..i]["Border"]:StripTextures()
-				end
-
-				_G["DropDownList"..i.."Backdrop"]:SetTemplate("Transparent")
-				_G["DropDownList"..i.."MenuBackdrop"]:SetTemplate("Transparent")
 			end
 		end)
 
@@ -177,7 +175,8 @@ SkinBlizzUI:SetScript("OnEvent", function(_, _, addon)
 
 				button.backdrop:Hide()
 
-				if not button.notCheckable then
+				local texture = check:GetTexture()
+				if not button.notCheckable and texture == 375502 then
 					uncheck:SetTexture()
 					local _, co = check:GetTexCoord()
 					if co == 0 then
@@ -189,7 +188,7 @@ SkinBlizzUI:SetScript("OnEvent", function(_, _, addon)
 					else
 						check:SetTexture(C.media.blank)
 						check:SetVertexColor(1, 0.82, 0, 0.8)
-						check:SetSize(5, 5)
+						check:SetSize(6, 6)
 						check:SetDesaturated(false)
 						button.backdrop:SetOutside(check)
 					end
