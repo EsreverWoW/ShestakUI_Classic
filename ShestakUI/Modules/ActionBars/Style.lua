974b23230f6423ec193d08f728b41fe2e5f4caf8
Wetxius <Wetxius@users.noreply.github.com>
2023-01-06 10:42:51 +0300

    [ActionBars] Fix ActionButton1.SpellActivationAlert size.
    # Conflicts:
    #	ShestakUI/Modules/ActionBars/Style.lua

--- ShestakUI/Modules/ActionBars/Style.lua
+++ ShestakUI/Modules/ActionBars/Style.lua
@@ -288,6 +288,12 @@ frame:SetScript("OnEvent", function(self, event)
 	if T.Mainline then
 		StyleNormalButton(ExtraActionButton1)
 	end
+
+	local spellAlert = ActionButton1.SpellActivationAlert
+	if spellAlert then
+		spellAlert:SetSize((C.actionbar.editor and C.actionbar.bar1_size or C.actionbar.button_size) * 1.4, (C.actionbar.editor and C.actionbar.bar1_size or C.actionbar.button_size) * 1.4)
+		ActionButton_HideOverlayGlow(ActionButton1)
+	end
 end)
 
 local function SetupFlyoutButton(button, self)
