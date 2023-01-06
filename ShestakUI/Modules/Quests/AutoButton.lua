ab8418eb976024af7a18ab6d0e12634d15674302
Wetxius <Wetxius@users.noreply.github.com>
2023-01-06 10:42:31 +0300

    [AutoButton] Fix error in combat.

--- ShestakUI/Modules/Quests/AutoButton.lua
+++ ShestakUI/Modules/Quests/AutoButton.lua
@@ -160,7 +160,7 @@ local function startScanningBags()
 				end)
 
 				AutoButton:SetScript("OnLeave", GameTooltip_Hide)
-				AutoButton:SetID(itemID)
+				AutoButton.id = itemID
 
 				AutoButtonShow(itemName)
 			end
@@ -191,5 +191,5 @@ end)
 -- Temp hide quest item by middle-click
 T.startScanningBags = startScanningBags
 
-local macro = "/run local T = unpack(ShestakUI) T.ABItemsIgnore[AutoButton:GetID()] = true T.startScanningBags() C_Timer.After(0.05, function() AutoButton:SetButtonState('NORMAL') end)"
+local macro = "/run local T = unpack(ShestakUI) T.ABItemsIgnore[AutoButton.id] = true T.startScanningBags() C_Timer.After(0.05, function() AutoButton:SetButtonState('NORMAL') end)"
 AutoButton:SetAttribute("macrotext3", macro)
