4de237a3fa5f4159f5f9ae8b233084baa4d63122
Wetxius <Wetxius@users.noreply.github.com>
2023-03-25 10:20:19 +0300

    Updated oUF.
    # Conflicts:
    #	ShestakUI/Libs/oUF/Blizzard.lua
    #	ShestakUI/Libs/oUF/Elements/Tags.lua

--- ShestakUI/Libs/oUF/Blizzard.lua
+++ ShestakUI/Libs/oUF/Blizzard.lua
@@ -7,10 +7,15 @@ local MAX_ARENA_ENEMIES = _G.MAX_ARENA_ENEMIES or 5
 -- sourced from FrameXML/TargetFrame.lua
 local MAX_BOSS_FRAMES = _G.MAX_BOSS_FRAMES or 5
 
+-- sourced from FrameXML/RaidFrame.lua
+local MEMBERS_PER_RAID_GROUP = _G.MEMBERS_PER_RAID_GROUP or 5
+
 -- sourced from FrameXML/PartyMemberFrame.lua
 local MAX_PARTY_MEMBERS = _G.MAX_PARTY_MEMBERS or 4
 
+local hookedFrames = {}
 local isArenaHooked = false
+local isBossHooked = false
 local isPartyHooked = false
 
 local hiddenParent = CreateFrame('Frame', nil, UIParent)
@@ -19,6 +24,12 @@ hiddenParent:Hide()
 
 local function insecureOnShow(self)
 	self:Hide()
+end
+
+local function resetParent(self, parent)
+	if(parent ~= hiddenParent) then
+		self:SetParent(hiddenParent)
+	end
 end
 
 local function handleFrame(baseName, doNotReparent)
@@ -35,6 +46,12 @@ local function handleFrame(baseName, doNotReparent)
 
 		if(not doNotReparent) then
 			frame:SetParent(hiddenParent)
+
+			if(not hookedFrames[frame]) then
+				hooksecurefunc(frame, 'SetParent', resetParent)
+
+				hookedFrames[frame] = true
+			end
 		end
 
 		local health = frame.healthBar or frame.healthbar or frame.HealthBar
@@ -71,6 +88,11 @@ local function handleFrame(baseName, doNotReparent)
 		if(totFrame) then
 			totFrame:UnregisterAllEvents()
 		end
+
+		local classPowerBar = frame.classPowerBar
+		if(classPowerBar) then
+			classPowerBar:UnregisterAllEvents()
+		end
 	end
 end
 
@@ -79,19 +101,6 @@ function oUF:DisableBlizzard(unit)
 
 	if(unit == 'player') then
 		handleFrame(PlayerFrame)
-
-		-- For the damn vehicle support:
-		PlayerFrame:RegisterEvent('PLAYER_ENTERING_WORLD')
-		if(not oUF:IsClassic() or oUF:IsWrath()) then
-			PlayerFrame:RegisterEvent('UNIT_ENTERING_VEHICLE')
-			PlayerFrame:RegisterEvent('UNIT_ENTERED_VEHICLE')
-			PlayerFrame:RegisterEvent('UNIT_EXITING_VEHICLE')
-			PlayerFrame:RegisterEvent('UNIT_EXITED_VEHICLE')
-		end
-
-		-- User placed frames don't animate
-		PlayerFrame:SetUserPlaced(true)
-		PlayerFrame:SetDontSavePosition(true)
 	elseif(unit == 'pet') then
 		handleFrame(PetFrame)
 	elseif(unit == 'target') then
@@ -107,12 +116,22 @@ function oUF:DisableBlizzard(unit)
 	elseif(oUF:IsClassic() and unit == 'targettarget') then
 		handleFrame(TargetFrameToT)
 	elseif(unit:match('boss%d?$')) then
-		local id = unit:match('boss(%d)')
-		if(id) then
-			handleFrame('Boss' .. id .. 'TargetFrame')
-		else
+		if(not isBossHooked) then
+			isBossHooked = true
+
+			-- it's needed because the layout manager can bring frames that are
+			-- controlled by containers back from the dead when a user chooses
+			-- to revert all changes
+			-- for now I'll just reparent it, but more might be needed in the
+			-- future, watch it
+			handleFrame(BossTargetFrameContainer)
+
+			-- do not reparent frames controlled by containers, the vert/horiz
+			-- layout code will go insane because it won't be able to calculate
+			-- the size properly, 0 or negative sizes in turn will break the
+			-- layout manager, fun...
 			for i = 1, MAX_BOSS_FRAMES do
-				handleFrame('Boss' .. i .. 'TargetFrame')
+				handleFrame('Boss' .. i .. 'TargetFrame', true)
 			end
 		end
 	elseif(unit:match('party%d?$')) then
@@ -129,10 +148,14 @@ function oUF:DisableBlizzard(unit)
 			if(not isPartyHooked) then
 				isPartyHooked = true
 
-				PartyFrame:UnregisterAllEvents()
+				handleFrame(PartyFrame)
 
 				for frame in PartyFrame.PartyMemberFramePool:EnumerateActive() do
-					handleFrame(frame)
+					handleFrame(frame, true)
+				end
+
+				for i = 1, MEMBERS_PER_RAID_GROUP do
+					handleFrame('CompactPartyFrameMember' .. i)
 				end
 			end
 		end
@@ -158,26 +181,28 @@ function oUF:DisableBlizzard(unit)
 				SetCVar('showArenaEnemyFrames', '0')
 				SetCVar('showArenaEnemyPets', '0')
 
-				-- but still UAE all containers
-				ArenaEnemyFramesContainer:UnregisterAllEvents()
-				ArenaEnemyPrepFramesContainer:UnregisterAllEvents()
-				ArenaEnemyMatchFramesContainer:UnregisterAllEvents()
+				-- but still UAE and hide all containers
+				handleFrame(ArenaEnemyFramesContainer)
+				handleFrame(ArenaEnemyPrepFramesContainer)
+				handleFrame(ArenaEnemyMatchFramesContainer)
 
 				for i = 1, MAX_ARENA_ENEMIES do
-					handleFrame('ArenaEnemyMatchFrame' .. i)
-					handleFrame('ArenaEnemyPrepFrame' .. i)
+					handleFrame('ArenaEnemyMatchFrame' .. i, true)
+					handleFrame('ArenaEnemyPrepFrame' .. i, true)
 				end
 			end
 		end
-	elseif(unit:match('nameplate%d+$')) then
-		local frame = C_NamePlate.GetNamePlateForUnit(unit)
-		if(frame and frame.UnitFrame) then
-			if(not frame.UnitFrame.isHooked) then
-				frame.UnitFrame:HookScript('OnShow', insecureOnShow)
-				frame.UnitFrame.isHooked = true
-			end
-
-			handleFrame(frame.UnitFrame, true)
-		end
 	end
+end
+
+function oUF:DisableNamePlate(frame)
+	if(not(frame and frame.UnitFrame)) then return end
+	if(frame.UnitFrame:IsForbidden()) then return end
+
+	if(not frame.UnitFrame.isHooked) then
+		frame.UnitFrame:HookScript('OnShow', insecureOnShow)
+		frame.UnitFrame.isHooked = true
+	end
+
+	handleFrame(frame.UnitFrame, true)
 end
