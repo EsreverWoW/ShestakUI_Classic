af063fda91eae2fc445873fa9444d6aa1dc044d2
Wetxius <Wetxius@users.noreply.github.com>
2023-04-20 10:14:59 +0300

    [10.1.0] Prevent error in NoTaint2.
    # Conflicts:
    #	ShestakUI/Modules/Blizzard/Fixes.lua

--- ShestakUI/Modules/Blizzard/Fixes.lua
+++ ShestakUI/Modules/Blizzard/Fixes.lua
@@ -182,7 +182,7 @@ if T.Mainline then
 			local stack = debugstack(4)
 			--call from UIParent.lua if ( not frame or frame:IsShown() ) then
 			--different when hooked
-			if stack:find('[string "=[C]"]: in function `ShowUIPanel\'\n', 1, true) then
+			if stack and stack:find('[string "=[C]"]: in function `ShowUIPanel\'\n', 1, true) then
 				cleanAll()
 			end
 		end)
