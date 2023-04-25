8ed57aca0f29f5528110667b7e609b7709bafb5a
Wetxius <wetxius@gmail.com>
2023-04-25 14:31:33 +0300

    Use less annoying sound for SafariHat announce.

--- ShestakUI/Modules/Announcements/SafariHat.lua
+++ ShestakUI/Modules/Announcements/SafariHat.lua
@@ -56,7 +56,7 @@ announceFrame:SetScript("OnEvent", function(_, event)
 				end
 			end
 			if maxlevel then return end
-			PlaySound(SOUNDKIT.RAID_WARNING, "master")
+			PlaySound(12867, "master")
 			RaidNotice_AddMessage(RaidWarningFrame, RESISTANCE_NONE.." "..GetSpellLink(158486).."!", ChatTypeInfo["RAID_WARNING"])
 			print("|cffff3300"..RESISTANCE_NONE.." "..GetSpellLink(158486).."|cffff3300!|r")
 		end
