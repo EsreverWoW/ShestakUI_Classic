cacf0d867f34ec5c06b435247151e0ea852c2e29
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2023-11-05 19:05:22 -0600

    Fixed Vehicle mouseover. pt. 2

--- ShestakUI/Modules/Blizzard/Vehicle.lua
+++ ShestakUI/Modules/Blizzard/Vehicle.lua
@@ -19,19 +19,37 @@ end)
 --	Vehicle indicator on mouseover
 ----------------------------------------------------------------------------------------
 if C.general.vehicle_mouseover then
-	local function VehicleSeatMouseover(self, vehicleID)
-		if self:IsShown() then
-			self:SetAlpha(0)
-			self:HookScript("OnEnter", function() self:SetAlpha(1) end)
-			self:HookScript("OnLeave", function() self:SetAlpha(0) end)
+	if T.Classic then
+		local function VehicleSeatMouseover(vehicleID)
+			if VehicleSeatIndicator:IsShown() then
+				VehicleSeatIndicator:SetAlpha(0)
+				VehicleSeatIndicator:HookScript("OnEnter", function() VehicleSeatIndicator:SetAlpha(1) end)
+				VehicleSeatIndicator:HookScript("OnLeave", function() VehicleSeatIndicator:SetAlpha(0) end)
 
-			local _, numSeat = GetVehicleUIIndicator(vehicleID)
-			for i = 1, numSeat do
-				local b = _G["VehicleSeatIndicatorButton"..i]
-				b:HookScript("OnEnter", function() self:SetAlpha(1) end)
-				b:HookScript("OnLeave", function() self:SetAlpha(0) end)
+				local _, numSeat = GetVehicleUIIndicator(vehicleID)
+				for i = 1, numSeat do
+					local b = _G["VehicleSeatIndicatorButton"..i]
+					b:HookScript("OnEnter", function() VehicleSeatIndicator:SetAlpha(1) end)
+					b:HookScript("OnLeave", function() VehicleSeatIndicator:SetAlpha(0) end)
+				end
 			end
 		end
+		hooksecurefunc("VehicleSeatIndicator_SetUpVehicle", VehicleSeatMouseover)
+	else
+		local function VehicleSeatMouseover(self, vehicleID)
+			if self:IsShown() then
+				self:SetAlpha(0)
+				self:HookScript("OnEnter", function() self:SetAlpha(1) end)
+				self:HookScript("OnLeave", function() self:SetAlpha(0) end)
+
+				local _, numSeat = GetVehicleUIIndicator(vehicleID)
+				for i = 1, numSeat do
+					local b = _G["VehicleSeatIndicatorButton"..i]
+					b:HookScript("OnEnter", function() self:SetAlpha(1) end)
+					b:HookScript("OnLeave", function() self:SetAlpha(0) end)
+				end
+			end
+		end
+		hooksecurefunc(VehicleSeatIndicator, "SetupVehicle", VehicleSeatMouseover)
 	end
-	hooksecurefunc(VehicleSeatIndicator, "SetupVehicle", VehicleSeatMouseover)
 end
