b8b6bdcb7f2d412980ef67e94240641b3f7c0569
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2023-11-26 16:57:13 -0600

    [Bags] Adjust frame strata / frame level pt. 2

--- ShestakUI/Config/DataText.lua
+++ ShestakUI/Config/DataText.lua
@@ -146,7 +146,7 @@ LPSTAT_CONFIG = {
 		tip_anchor = cBN and "TOPRIGHT" or C.bag.enable and "TOPRIGHT" or "BOTTOMRIGHT",
 		tip_x = cBN and 0 or C.bag.enable and -50 or -21,
 		tip_y = cBN and 85 or C.bag.enable and 0 or 20,
-		strata = cBN and "DIALOG" or "MEDIUM",
+		strata = cBN and "DIALOG" or "HIGH",
 		level = 10
 	},
 -- Top block
