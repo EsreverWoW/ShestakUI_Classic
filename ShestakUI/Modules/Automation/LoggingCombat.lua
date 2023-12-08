28b623faf8c667c3dead7bc60ba8725d6316775a
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2023-12-08 17:21:31 -0600

    [Automation] LoggingCombat: Fix for Classic raids since they seemingly return "party" with 2nd return of IsInInstance() pt. 2

--- ShestakUI/Modules/Automation/LoggingCombat.lua
+++ ShestakUI/Modules/Automation/LoggingCombat.lua
@@ -8,8 +8,8 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", function()
 	local _, instanceType = IsInInstance()
-	local _, _, difficultyID = GetInstanceInfo()
-	if (instanceType == "raid" or (T.SoD and difficultyID and difficultyID == 198)) and IsInRaid(LE_PARTY_CATEGORY_HOME) then
+	local _, _, difficultyID_, _, _, _, instanceID = GetInstanceInfo()
+	if (instanceType == "raid" or (T.SoD and difficultyID_ == 198 or T.SoD and (instanceID == 48))) and IsInRaid(LE_PARTY_CATEGORY_HOME) then
 		if not LoggingCombat() then
 			LoggingCombat(1)
 			print("|cffffff00"..COMBATLOGENABLED.."|r")
