74d39e5311373cde0fc88227d75295732b73c7d8
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2024-04-26 19:56:02 -0500

    [Quests] WatchFrame for Wrath/Cataclysm

--- ShestakUI/Modules/Quests/ObjectiveTracker_Vanilla.lua
+++ ShestakUI/Modules/Quests/ObjectiveTracker_Vanilla.lua
@@ -1,5 +1,4 @@
 local T, C, L = unpack(ShestakUI)
-if T.Wrath then return end -- FIXME
 if Questie and Questie.db and Questie.db.profile and Questie.db.profile.trackerEnabled then return end
 
 ----------------------------------------------------------------------------------------
